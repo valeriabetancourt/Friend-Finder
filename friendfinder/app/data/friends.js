b860629ad263bac10669f236a57dabfc40e08921
Valeria Betancourt <valeriabetancourt@Valerias-MacBook-Pro.local>
2019-02-10 16:07:26 -0600

    Added final changes to the hw

---
+++ friendfinder/app/data/friends.js
@@ -0,0 +1,38 @@
+var friends = [
+  {
+    name: "Chandler Bing",
+    photo:
+      "https://pixel.nymag.com/imgs/daily/vulture/2017/03/30/30-chandler-bing.w330.h330.jpg",
+    scores: [4, 3, 1, 1, 1, 5, 5, 5, 5, 5]
+  },
+  {
+    name: "Monica Geller",
+    photo:
+      "http://s3-eu-west-1.amazonaws.com/grazia-cdn/wp-content/uploads/2018/10/473647375a0c0ca3e4025aa16eb4cb21.jpg",
+    scores: [2, 2, 2, 4, 5, 3, 4, 5, 2, 5]
+  },
+  {
+    name: "Ross Geller",
+    photo:
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/nicolas-cage-friends-ross-geller-1549451710.jpg",
+    scores: [3, 2, 5, 2, 4, 2, 3, 1, 1, 4]
+  },
+  {
+    name: "Rachel Green",
+    photo: "https://s2.r29static.com//bin/entry/5db/x,80/1839169/image.jpg",
+    scores: [2, 1, 1, 5, 3, 2, 2, 5, 2, 5]
+  },
+  {
+    name: "Joey Tribbiani",
+    photo:
+      "https://i.pinimg.com/originals/b1/20/f7/b120f79df4e5f7aee241c87422322316.jpg",
+    scores: [5, 4, 2, 1, 1, 3, 5, 5, 5, 5]
+  },
+  {
+    name: "Phoebe Buffay",
+    photo:
+      "http://static1.squarespace.com/static/5a6fc8452278e782e8ee0133/5a93593be2c483bcb2b791a1/5bd780054785d34ca276f557/1547600840899/Phoebe+Buffay.jpg?format=1500w",
+    scores: [2, 5, 3, 2, 2, 4, 1, 5, 1, 5]
+  }
+];
+module.exports = friends;
